4d4fd34c6e16029ab6c0a0e4ad20d5cf6bed13a1
Gennadiy Bolkunov <gennadiy.bolkunov@gs-labs.ru>
2026-02-11 17:58:02 +0300

    fix lib

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,7 +50,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
-    implementation("com.infobip:google-webrtc:1.0.45036")
+    implementation("io.getstream:stream-webrtc-android:1.3.10")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
